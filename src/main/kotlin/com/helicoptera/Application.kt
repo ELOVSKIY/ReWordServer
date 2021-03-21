dce7b9ae38aa19a2a87ff22972e7aad6fe69b3bb
Stanislav Elovsky <helicoptera@yandex-team.ru>
2021-03-21 23:57:28 +0300

    initial commit

---
+++ src/main/kotlin/com/helicoptera/Application.kt
@@ -0,0 +1,97 @@
+package com.helicoptera
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+/**
+ * Please note that you can use any other name instead of *module*.
+ * Also note that you can have more then one modules in your application.
+ * */
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+    routing {
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+    routing {
+        get("/html-dsl") {
+            call.respondHtml {
+                body {
+                    h1 { +"HTML" }
+                    ul {
+                        for (n in 1..10) {
+                            li { +"$n" }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    routing {
+        get("/styles.css") {
+            call.respondCss {
+                body {
+                    backgroundColor = Color.darkBlue
+                    margin(0.px)
+                }
+                rule("h1.page-title") {
+                    color = Color.white
+                }
+            }
+        }
+    }
+    routing {
+        get("/html-css-dsl") {
+            call.respondHtml {
+                head {
+                    link(rel = "stylesheet", href = "/styles.css", type = "text/css")
+                }
+                body {
+                    h1(classes = "page-title") {
+                        +"Hello from Ktor!"
+                    }
+                }
+            }
+        }
+    }
+}
+
+suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
+    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+}
+
